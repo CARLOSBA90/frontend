45273c1568708e74326e3c4de1d6cf906fb8997e
CARLOSBA90 <cepr0190@gmail.com>
2021-08-27 20:06:23 -0300

    configuracion final plantilla base

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,6 +3,7 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable, throwError } from 'rxjs';
 import { catchError, retry } from 'rxjs/operators';
+import { RestService } from './rest.service';
 
 @Component({
   selector: 'app-root',
@@ -11,21 +12,28 @@ import { catchError, retry } from 'rxjs/operators';
 })
 
 @Injectable()
-export class AppComponent {
+export class AppComponent{
   title = 'Titulo';
   text = 'Texto de ejemplo';
 
- constructor(private http: HttpClient){}
-
+ //constructor(private http: HttpClient){}
+ constructor(private RestService: RestService){}
  ngOnInit (){
-
-   this.http.get("http://localhost:8080/").subscribe((resp:any) =>{
+   this.cargarData();
+ /*  this.http.get("http://localhost:8080/").subscribe((resp:any) =>{
          this.text = resp;
    }),
    (error:any) =>{
      console.log(error)
-   }
+   }*/
    
+ }
+
+ public cargarData(){
+    this.RestService.get("http://localhost:8080/")
+  .subscribe((respuesta:any) => {
+    console.log(respuesta)
+  })
  }
 
 }
