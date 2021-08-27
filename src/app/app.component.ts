2804c343a1d53dc39296598288539aaf9b061a25
CARLOSBA90 <cepr0190@gmail.com>
2021-08-27 14:49:50 -0300

    Plantilla base, integracion con springboot(HTTP CLIENT)

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,31 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable, throwError } from 'rxjs';
+import { catchError, retry } from 'rxjs/operators';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
+@Injectable()
 export class AppComponent {
-  title = 'frontend';
+  title = 'Titulo';
+  text = 'Texto de ejemplo';
+
+ constructor(private http: HttpClient){}
+
+ ngOnInit (){
+
+   this.http.get("http://localhost:8080/").subscribe((resp:any) =>{
+         this.text = resp;
+   }),
+   (error:any) =>{
+     console.log(error)
+   }
+   
+ }
+
 }
